b170a23795d32db92e6d4b11a55dbf67d676e127
Sokolnik21 <m.sokol.vog@gmail.com>
2018-05-03 15:27:33 +0200

    Have all needed functionality. To do: add functions in Server.c to make code look simpler

--- laboratories6/exercise1/Client.c
+++ laboratories6/exercise1/Client.c
@@ -19,12 +19,22 @@ void removeQueue(void);
 int GLOBAL_CLIENT_MSG_ID;
 
 int main(int argc, char * argv[]) {
+  /* Checking amount of arguments */
+  if(argc != 2) {
+    printf("Wrong amount of arguments\n");
+    return -1;
+  }
+
   /* Variables */
+  char filePath[PATH_MAX];
   key_t serverQueueKey;
   int msgidS;
   int msgidC;
   struct msgbuf communicat;
   struct msgbuf serverMsg;
+
+  /* Parsing variables */
+  strcpy(filePath, argv[1]);
 
   /* Almost-Garbage collector */
   atexit(removeQueue);
@@ -62,25 +72,52 @@ int main(int argc, char * argv[]) {
   if(msgrcv(msgidC, &serverMsg, sizeof(serverMsg.mtext), GET_LATEST, MSG_NOERROR) == -1) {
     endWithError(-1);
   }
-  printf("%s. client\n", serverMsg.mtext);
+  printf("%s. client", serverMsg.mtext);
 
   /* SEND MESSAGES START */
-  while(1) {
-    /* Send signal and wait for response */
-    communicat.mtype = MIRROR;
-    communicat.pid = getpid();
-    strcpy(communicat.mtext, "14*93");
-    strcpy(communicat.mtext, "Ziggy plays guitar");
-    if(msgsnd(msgidS, &communicat, MSGMAX, MSG_NOERROR) == -1) {
-      endWithError(-1);
+  /* Setting file handler */
+  FILE * f = fopen(filePath, "r");
+  if (f == NULL) {
+    printf("Error opening file!\n");
+    exit(1);
+  }
+
+  /* Get every line from file and parse it */
+  char * line = malloc(PATH_MAX * sizeof(char));
+  size_t len = 0;
+  ssize_t readLine;
+  while ((readLine = getline(&line, &len, f)) != -1) {
+    switch(line[0]) {
+      case 'M' :
+        communicat.mtype = MIRROR;
+        strcpy(communicat.mtext, line + 7);
+        if(msgsnd(msgidS, &communicat, MSGMAX, MSG_NOERROR) == -1) {
+          endWithError(-1);
+        }
+        break;
+      case 'C' :
+        communicat.mtype = CALC;
+        strcpy(communicat.mtext, line + 5);
+        if(msgsnd(msgidS, &communicat, MSGMAX, MSG_NOERROR) == -1) {
+          endWithError(-1);
+        }
+        break;
+      case 'T' :
+        communicat.mtype = TIME;
+        if(msgsnd(msgidS, &communicat, MSGMAX, MSG_NOERROR) == -1) {
+          endWithError(-1);
+        }
+      case 'E' :
+        communicat.mtype = END;
+        if(msgsnd(msgidS, &communicat, MSGMAX, MSG_NOERROR) == -1) {
+          endWithError(-1);
+        }
     }
     if(msgrcv(msgidC, &serverMsg, sizeof(serverMsg.mtext), GET_LATEST, MSG_NOERROR) == -1) {
       endWithError(-1);
     }
     printf("%s\n", serverMsg.mtext);
     sleep(1);
-
-    exit(0);
   }
   /* SEND MESSAGES END */
 
